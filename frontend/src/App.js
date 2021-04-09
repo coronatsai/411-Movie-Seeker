a2ba9ce2d3dc3ff4b51055b005fe2abd32124a97
Corona Tsai <cstsai2@illinois.edu>
2021-04-09 16:54:58 -0500

    Still does not work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {Component, useState, useEffect} from "react";
+import React, {Component, useState, useEffect} from 'react';
 import Axios from 'axios';
 
 function App() {
