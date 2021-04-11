11411aa041077d76998584b035467077e0cab489
Corona Tsai <cstsai2@illinois.edu>
2021-04-10 20:13:03 -0500

    Queries all work!

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 const cors = require("cors");
 
 var db = mysql.createConnection({
@@ -14,33 +14,78 @@ var db = mysql.createConnection({
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-});
+  });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/',(require, response) => {
-//   const sqlInsert = "INSERT INTO 'subscribed' ('Name', 'User_id') VALUES ('Testing', '0.0');";
-//   db.query(sqlInsert, (err, result) => {
-//     response.send("Hello world!!!");
-//   })
-// })
 
-app.post('/api/insert', (require, response) => {
-  const Name = require.body.Name;
-  const User_id = require.body.User_id;
-  console.log("before insert attempt");
-  var sqlInsert = "INSERT INTO 'subscribed' ('Name', 'User_id') VALUES (?,?);";
-  db.query(sqlInsert, [Name, User_id], function(err, result) {
-    if (err) throw err;
-    console.log(err);
-    respond.send("insert a row");
-    console.log("reached inside");
+app.delete(`/api/delete/:movieIDWatchedMovie/:userIDWatchedMovie`, (require, response) => {
+  const movieIDWatchedMovie = require.body.movieIDWatchedMovie;
+  const userIDWatchedMovie = require.body.userIDWatchedMovie;
+
+  const sqlDelete = "DELETE FROM watched_movie WHERE ID = ? AND User_id = ?;";
+  db.query(sqlDelete, [movieIDWatchedMovie, userIDWatchedMovie], (err, result) => {
+    console.log(result);
+    response.send("deleted a row from watched_movie");
   })
-
 });
-console.log("after insert attempt");
+
+app.post(`/api/insertNewWatchMovie`, (require, response) => {
+    const movieIDWatchedMovie = require.body.movieIDWatchedMovie;
+    const userIDWatchedMovie = require.body.userIDWatchedMovie;
+    const dateWatchedMovie = require.body.dateWatchedMovie;
+    const timeWatchedMovie = require.body.timeWatchedMovie;
+    const ratingWatchedMovie = require.body.ratingWatchedMovie;
+    var sql = "INSERT INTO watched_movie (ID,User_id,date,time,rating) VALUES (?, ?,?,?,?);";
+    db.query(sql, [movieIDWatchedMovie, userIDWatchedMovie,dateWatchedMovie,timeWatchedMovie,ratingWatchedMovie], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        response.send("pls work... :(");
+        console.log("test 1 completed");
+      });
+})
+
+app.get(`/api/searchMovieTitles`, (request, result) => {
+  const movieSearchNameKeyword = request.query.movieSearchNameKeyword;
+  const sqlRead = "SELECT title, rating FROM Movies WHERE title LIKE CONCAT('%',?,'%');";
+  db.query(sqlRead, [movieSearchNameKeyword], (err, results, fields) => {
+    if (err) throw err;
+    result.send(results);
+  });
+})
+
+app.get(`/api/searchMoviesWatched`, (request, result) => {
+  const userIDWatchedMovie = request.query.userIDWatchedMovie;
+  const sqlRead = "SELECT m.title, w.rating FROM Movies m JOIN watched_movie w on m.ID = w.ID WHERE w.User_id = ?;";
+  db.query(sqlRead, [userIDWatchedMovie], (err, results, fields) => {
+    if (err) throw err;
+    result.send(results);
+  });
+})
+
+app.get("/api/getNumberOfMoviesPerGenre", (request, result) => {
+  const sqlRead = "SELECT g.genre_name as name, count(w.time) as num FROM watched_movie w JOIN belongs_to_Movie g ON (w.ID = g.ID) GROUP BY g.genre_name;";
+  db.query(sqlRead,[],(err,results, fields) => {
+    if (err) throw err;
+    result.send(results);
+  })
+})
+
+app.put("/api/updateReview", (require, response) => {
+  const movieIDWatchedMovie = require.body.movieIDWatchedMovie;
+  const userIDWatchedMovie = require.body.userIDWatchedMovie;
+  const newMovieRating = require.body.newMovieRating;
+  const sqlUpdate = "UPDATE watched_movie SET rating = ? WHERE ID = ? AND User_id = ?;";
+  db.query(sqlUpdate, [newMovieRating, movieIDWatchedMovie, userIDWatchedMovie], (err, result) => {
+    if (err) throw err;
+    response.send("Update successful");
+    console.log(err);
+  });
+})
+
+
 app.listen(3002, () => {
   console.log("running on port 3002");
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,39 +3,202 @@ import React, {Component, useState, useEffect} from 'react';
 import Axios from 'axios';
 
 function App() {
-  const [serviceName, setServiceName] = useState('');
-  const [userID, setUserID] = useState('');
+  // const [serviceName, setServiceName] = useState('');
+  // const [userID, setUserID] = useState('');
 
-  const submitNewStreaming = () => {
-    Axios.post('https://localhost:3002/api/insert', {
-      Name: serviceName,
-      User_id: userID,
-    }).then(() => {
-      alert('successful insert')
+
+  /* Watch Movie*/
+  const [movieIDWatchedMovie, setMovieIDWatchedMovie] = useState('');
+  const [userIDWatchedMovie, setUserIDWatchedMovie] = useState('');
+  const [dateWatchedMovie, setDateWatchedMovie] = useState('');
+  const [timeWatchedMovie, setTimeWatchedMovie] = useState('');
+  const [ratingWatchedMovie, setRatingWatchedMovie] = useState('');
+
+  /* Get Movies watched by User*/
+  const [returnMovieWatchList, setReturnMovieWatchList] = useState([]);
+
+  /* Search Movies watched by User containing substring*/
+  const [movieSearchNameKeyword, setMovieSearchNameKeyword] = useState('');
+  const [returnMovieSearchNameKeyword, setReturnMovieSearchNameKeyword] = useState([]);
+
+  /* Update a user's movie rating */
+  const [newMovieRating, setNewMovieRating] = useState('');
+
+  /* Adv query to get number of movies per genre */
+  const [returnNumMoviesPerGenre, setReturnNumMoviesPerGenre] = useState([]);
+
+
+  const getNumberOfMoviesPerGenre = () => {
+      Axios.get('http://localhost:3002/api/getNumberOfMoviesPerGenre'
+    ).then((response) => {
+      setReturnNumMoviesPerGenre(response.data)
     })
   };
 
+  const getMovieSearcNameKeyword = () => {
+    Axios.get('http://localhost:3002/api/searchMovieTitles', {
+      params: {
+        movieSearchNameKeyword:movieSearchNameKeyword
+      }
+    }).then((response) => {
+      setReturnMovieSearchNameKeyword(response.data)
+    })
+  };
+
+  const getMoviesWatchedByUser = () => {
+    Axios.get('http://localhost:3002/api/searchMoviesWatched', {
+      params: {
+        userIDWatchedMovie:userIDWatchedMovie
+      }
+    }).then((response) => {
+      setReturnMovieWatchList(response.data)
+    })
+  };
+
+  const updateMovieWatch = () => {
+    Axios.put(`http://localhost:3002/api/updateReview`, {
+      movieIDWatchedMovie: movieIDWatchedMovie,
+      userIDWatchedMovie: userIDWatchedMovie,
+      newMovieRating: newMovieRating
+    });
+    setNewMovieRating("")
+  };
+
+  const submitNewWatchMovie = () => {
+    Axios.post(`http://localhost:3002/api/insertNewWatchMovie`, {
+      movieIDWatchedMovie: movieIDWatchedMovie,
+      userIDWatchedMovie: userIDWatchedMovie,
+      dateWatchedMovie: dateWatchedMovie,
+      timeWatchedMovie: timeWatchedMovie,
+      ratingWatchedMovie: ratingWatchedMovie,
+    }).then(() => {
+      alert('success insert')
+    });
+
+  };
+
+  const deleteWatchMovie = () => {
+    Axios.delete(`https://localhost:3002/api/delete/${movieIDWatchedMovie}/${userIDWatchedMovie}`,
+      {data: {
+      movieIDWatchedMovie:movieIDWatchedMovie,
+      userIDWatchedMovie:userIDWatchedMovie
+    }})
+  };
+
+
+
   return (
-    <div className="App">
-      <h1> Movie Seeker CRUD:</h1>
+   <div className="App">
+     <h1> MOVIE SEEKER CRUD</h1>
+     <div className = "form">
+
+     <h2> Display Movies Watched by User </h2>
+     <label> User ID: </label>
+       <input type= "text" name = "userIDWatchedMovie" onChange={(e) => {
+         setUserIDWatchedMovie(e.target.value)
+       }}/>
+
+     <button onClick = {getMoviesWatchedByUser}> Show Me! </button>
+
+     {returnMovieWatchList.map((val) => {
+       return(
+         <div className = "card">
+         <p> Movie Name: {val.title} </p>
+         <p> User's Rating: {val.rating} </p>
+         </div>
+       );
+     })}
+
+     <h2> Update a Movie Rating </h2>
+     <label>Movie ID: </label>
+     <input type= "text" name = "movieIDWatchedMovie" onChange={(e) => {
+       setMovieIDWatchedMovie(e.target.value)
+       }}/>
+      <p></p>
+     <label>User ID: </label>
+       <input type= "text" name = "userIDWatchedMovie" onChange={(e) => {
+       setUserIDWatchedMovie(e.target.value)
+       }}/><p></p>
+     <label>UPDATED Rating (0-10): </label>
+       <input type= "text" name = "newMovieRating" onChange={(e) => {
+       setNewMovieRating(e.target.value)
+       }}/><p></p>
+
+    <button onClick = {updateMovieWatch}> Update </button>
 
 
-      <div className= "form">
-        <label>Streaming Platform Name:</label>
-        <input type="text" name="serviceName" onChange={(e) =>{
-          setServiceName(e.target.value)
-        }} />
-        <label>User ID:</label>
-        <input type="text" name="userID" onChange={(e) => {
-          setUserID(e.target.value)
-        }}/>
+     <h2> Rate A Movie That You Watched:</h2>
+     <label>Movie ID: </label>
+     <input type= "text" name = "movieIDWatchedMovie" onChange={(e) => {
+       setMovieIDWatchedMovie(e.target.value)
+       }}/>
+      <p></p>
+     <label>User ID: </label>
+       <input type= "text" name = "userIDWatchedMovie" onChange={(e) => {
+       setUserIDWatchedMovie(e.target.value)
+       }}/><p></p>
+     <label>Date Watched (YYYY-M-D): </label>
+       <input type= "text" name = "dateWatchedMovie" onChange={(e) => {
+       setDateWatchedMovie(e.target.value)
+       }}/><p></p>
+     <label>Time Watched (0-24H:M): </label>
+       <input type= "text" name = "timeWatchedMovie" onChange={(e) => {
+       setTimeWatchedMovie(e.target.value)
+       }}/><p></p>
+     <label>Rating (0-10): </label>
+       <input type= "text" name = "ratingWatchedMovie" onChange={(e) => {
+       setRatingWatchedMovie(e.target.value)
+       }}/><p></p>
 
-        <button onClick = {submitNewStreaming}> Submit </button>
+    <button onClick = {submitNewWatchMovie}> submit </button>
 
-      </div>
+    <h2> Remove a Movie Rating:</h2>
+    <label>Movie ID: </label>
+    <input type= "text" name = "movieIDWatchedMovie" onChange={(e) => {
+      setMovieIDWatchedMovie(e.target.value)
+      }}/>
+     <p></p>
+    <label>User ID: </label>
+      <input type= "text" name = "userIDWatchedMovie" onChange={(e) => {
+      setUserIDWatchedMovie(e.target.value)
+      }}/><p></p>
 
-    </div>
-  );
+    <button onClick = {deleteWatchMovie}> DELETE </button>
+
+    <h2> Search Movie Titles </h2>
+    <label> Keyword: </label>
+      <input type= "text" name = "movieSearchNameKeyword" onChange={(e) => {
+        setMovieSearchNameKeyword(e.target.value)
+      }}/>
+
+    <button onClick = {getMovieSearcNameKeyword}> Search </button>
+
+    <p></p>
+
+    {returnMovieSearchNameKeyword.map((val) => {
+      return(
+        <div className = "card">
+        <p> Movie Name: {val.title} </p>
+        <p> Movie Rating: {val.rating} </p>
+        </div>
+      );
+    })}
+
+    <h2> Show Number of Movies per Genre </h2>
+    <button onClick = {getNumberOfMoviesPerGenre}> Show Me! </button>
+
+    <p></p>
+
+    {returnNumMoviesPerGenre.map((val) => {
+      return(
+        <div className = "card">
+        <p> Movie Genre: {val.name} Number of Titles: {val.num} </p>
+        </div>
+      );
+    })}
+
+   </div>
+   </div>
+ );
 }
-
 export default App;
